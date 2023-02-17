ecf6d0ce089df21960ada5ea41be1e17b193292d
EC2 Default User <ec2-user@ip-172-31-7-225.ap-northeast-1.compute.internal>
2023-02-17 07:59:50 +0000

    [update]bookers2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  def after_sign_in_path_for(resourse)
+    oo_path
+  end
+
+  # ログアウト後の遷移先
+  def after_sign_out_path_for(resourrce)
+    root_path
+  end
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
