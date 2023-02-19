ba488412be6c34a09cd0b88a133ffe1258f256ed
EC2 Default User <ec2-user@ip-172-31-7-225.ap-northeast-1.compute.internal>
2023-02-19 10:57:17 +0000

    [bootstrap_now]bookers2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,17 +6,16 @@ class ApplicationController < ActionController::Base
 
 
   def after_sign_in_path_for(resourse)
-    # サインイン後はどのページに遷移するか？引数設定忘れない！
+    flash[:notice] = "Welcome! You have signed up successfully."
+    # サインイン後はどのページに遷移するか？引数設定忘れない！render
     user_path(current_user.id)
-    # flash[:notice] = "Welcome! You have signed up successfully."
-    # コメントアウトするとブラウザ表示されるのに、解除でエラーになる？？
 
   end
 
   # ログアウト後の遷移先
   def after_sign_out_path_for(resource)
+    flash[:notice] = "Signed out successfully."
     root_path
-    # flash[:notice] = "Signed out successfully."
   end
 
   protected
